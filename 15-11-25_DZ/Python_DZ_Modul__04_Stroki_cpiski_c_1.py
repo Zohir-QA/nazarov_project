feeb812d4ff618437656d56762a0d50a37b2ad0a
@mail.ru <9026479948e@gmail.com>
2025-11-19 21:21:57 +0500

    попыточка

--- 15-11-25_DZ/Python_DZ_Modul__04_Stroki_cpiski_c_1.py
+++ 15-11-25_DZ/Python_DZ_Modul__04_Stroki_cpiski_c_1.py
@@ -7,19 +7,19 @@
 # else:
 #     print("Это не палиндром")
 # 2
-# text = input("Введите текст: ")
-# text_2 = input("Введите список зарезервированных слов: ").split() # разбивает введенную строку на отдельные слова
-# for i in text_2:
-#     text = text.replace(i, i.upper())
-# print(text)
-# 3
 text = input("Введите текст: ")
-text_1 = text.count('.')
-text_2 = text.count('!')
-text_3 = text.count('?')
-count = text_1 + text_2 + text_3
-if text[-1] != "." and text[-1] != "!" and text[-1] != "?":
-    count += 1
-    print("Количество предложений:", count)
-else:
-    print("Количество предложений:", count)
+text_2 = input("Введите список зарезервированных слов: ").split() # разбивает введенную строку на отдельные слова
+for i in text_2:
+    text = text.replace(i, i.upper())
+print(text)
+# 3
+# text = input("Введите текст: ")
+# text_1 = text.count('.')
+# text_2 = text.count('!')
+# text_3 = text.count('?')
+# count = text_1 + text_2 + text_3
+# if text[-1] != "." and text[-1] != "!" and text[-1] != "?":
+#     count += 1
+#     print("Количество предложений:", count)
+# else:
+#     print("Количество предложений:", count)
