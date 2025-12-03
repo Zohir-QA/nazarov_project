ae92cfd71e7544e7e9bb5a1b7a8dc9263218295a
@mail.ru <9026479948e@gmail.com>
2025-12-03 21:16:45 +0500

    50 % выполненной домашки

--- lecture/lection.py
+++ lecture/lection.py
@@ -665,6 +665,13 @@
 # пакеты - это папки с модулями
 # функций - print
 #
-
+fruits = ["яблоко", "апельсин", "банан"]
+empty_list = []
+empty_list_v2 = list()
+for i in fruits[1]:
+    for val in i[-1]:
+        print(val)
+        break
+    break
 
 
