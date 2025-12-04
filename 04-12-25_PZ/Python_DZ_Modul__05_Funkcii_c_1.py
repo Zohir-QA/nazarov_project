ad9fd58e00ca7a1bb098cfc3b2ed385450d2b118
student <zokhir@inbox.ru>
2025-12-04 21:15:57 +0500

    практическая ;(

--- 04-12-25_PZ/Python_DZ_Modul__05_Funkcii_c_1.py
+++ 04-12-25_PZ/Python_DZ_Modul__05_Funkcii_c_1.py
@@ -1,19 +1,33 @@
 # 3
-def kvadrat(dlina, simvol, number):
-    print(dlina * simvol)
-    if dlina > 1:
-        for i in range(dlina - 2):
-            if number == "True":
-                print(dlina * simvol)
-            else:
-                print(simvol + " " * (dlina - 2) + simvol)
-    print(dlina * simvol)
+# def kvadrat(dlina, simvol, peremen):
+#     if peremen == False:
+#         print(dlina * simvol)
+#         if dlina > 1:
+#             for i in range(dlina - 2):
+#                 print(simvol + " " * (dlina - 2) + simvol)
+#         print(dlina * simvol)
+#     else:
+#         for i in range (dlina):
+#             print(simvol * dlina)
+#
+# kvadrat(10, "*", False)
+#
+# 4
+# def function_min(number):
+#     val = min(number)
+#     print("Минимальное число:", val)
+#
+# my_number = input("Введите числа через пробел: ").split( )
+# function_min(my_number)
+# 5
+def range_limits(range_1, range_2):
+    val_1 = max(range_1, range_2)
+    val_2 = min(range_1, range_2)
+    print(f"Верхняя граница - {val_1}, а нижняя граница равна - {val_2}")
 
-
-
-
-kvadrat(10, "*", False)
-
+number_1 = int(input("Введите первое число диапазона: "))
+number_2 = int(input("Введите второе число диапазона: "))
+range_limits(number_1 , number_2)
 
 
 
