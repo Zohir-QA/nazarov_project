81f2016b8ae9bb6cf5c014e1eef82c5e4210eeac
@mail.ru <9026479948e@gmail.com>
2025-12-22 21:09:42 +0500

    ДОДЕЛАТЬ 18 И СДЕЛАТЬ 22

---
+++ DvaDva_OdinDva_DvaPati/seckond_package/second_module.py
@@ -0,0 +1,11 @@
+# 1
+# from DvaDva_OdinDva_DvaPati.first_packade.first_module import print_hello_word
+# print_hello_word()
+# 2
+# from DvaDva_OdinDva_DvaPati.first_packade import first_module
+# first_module.print_hello_word()
+# 3
+from DvaDva_OdinDva_DvaPati.first_packade.first_module import MyClass
+mu_klass = MyClass
+mu_klass.print_hello_word()
+MyClass.print_hello_word()
